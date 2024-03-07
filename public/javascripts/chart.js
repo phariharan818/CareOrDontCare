730652b9a7d9498b519386705cb79fba514407ee
isaxena02 <ishita.saxena1103@gmail.com>
2024-03-06 21:30:43 -0800

    static viz added

--- public/javascripts/chart.js
+++ public/javascripts/chart.js
@@ -31,34 +31,36 @@
 // });
 
 
-document.addEventListener('DOMContentLoaded', async () => {
-    const response = await fetch('/topCaredArticles');
-    const data = await response.json();
+// document.addEventListener('DOMContentLoaded', async () => {
+//     const response = await fetch('/topCaredArticles');
+//     const data = await response.json();
 
-    const titles = data.map(article => article.title);
-    const clickCounts = data.map(article => article.clickCount);
+//     const titles = data.map(article => article.title);
+//     const clickCounts = data.map(article => article.clickCount);
 
-    const chartContainer = document.getElementById('chartContainer');
+//     const chartContainer = document.getElementById('chartContainer');
 
-    const topTopics = chartContainer.getContext('2d');
-    new Chart(topTopics, {
-        type: 'bar',
-        data: {
-            labels: titles,
-            datasets: [{
-                label: 'Number of Clicks',
-                data: clickCounts,
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
-});
+//     const topTopics = chartContainer.getContext('2d');
+//     new Chart(topTopics, {
+//         type: 'bar',
+//         data: {
+//             labels: titles,
+//             datasets: [{
+//                 label: 'Number of Clicks',
+//                 data: clickCounts,
+//                 backgroundColor: 'rgba(75, 192, 192, 0.2)',
+//                 borderColor: 'rgba(75, 192, 192, 1)',
+//                 borderWidth: 1
+//             }]
+//         },
+//         options: {
+//             scales: {
+//                 y: {
+//                     beginAtZero: true
+//                 }
+//             }
+//         }
+//     });
+// });
+
+
